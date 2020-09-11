53b63c4a76de3a8113a7326d16726c22040663e9
James <pdillon222@gmail.com>
2020-09-11 08:14:37 -0700

    working on b-tree constructor for LeetCode list input

---
+++ Range_Sum_Of_BST_/py.d/.examples.py
@@ -0,0 +1,36 @@
+
+#Recursive implementation
+class RecurseSolution(object):
+    def rangeSumBST(self, root, L, R):
+        def dfs(node):
+            if node:
+                if L <= node.val <= R:
+                    self.ans += node.val
+                if L < node.val:
+                    dfs(node.left)
+                if node.val < R:
+                    dfs(node.right)
+
+        self.ans = 0
+        dfs(root)
+        return self.ans
+
+#Iterative implementation
+class IterateSolution(object):
+    def rangeSumBST(self, root, L, R):
+        ans = 0
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                if L <= node.val <= R:
+                    ans += node.val
+                if L < node.val:
+                    stack.append(node.left)
+                if node.val < R:
+                    stack.append(node.right)
+        return ans
+
+if __name__=="__main__":
+    sol_r = RecurseSolution()
+    sol_i = IterativeSolution()
--- Range_Sum_Of_BST_/py.d/tree_range_of_sum.py
+++ Range_Sum_Of_BST_/py.d/tree_range_of_sum.py
@@ -1,44 +1,41 @@
+# TODO - construct a method to build the tree from given list:
+# [10,5,15,3,7,13,18,1,null,6] => becomes
+# L=7, R=15 => 45
+'''
+                   10
+                 /    \
+                5     15
+              /   \  /   \
+             3    7 13   18
+           /     /
+          1     6
+'''
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
 class Solution:
-    def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+  def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+    return(root.val, root.left.left.left.val, root.left.right.left.val)
 
+if __name__=="__main__":
+  sol = Solution()
+  # [10,5,15,3,7,13,18,1,null,6], L=7, R=15 => 45
 
+  # define root left
+  ll = TreeNode(val=3, left=TreeNode(val=1))
+  lr = TreeNode(val=7, left=TreeNode(val=6))
+  l = TreeNode(val=5, left=ll, right=lr)
 
-'''
-#Recursive implementation
-class Solution(object):
-    def rangeSumBST(self, root, L, R):
-        def dfs(node):
-            if node:
-                if L <= node.val <= R:
-                    self.ans += node.val
-                if L < node.val:
-                    dfs(node.left)
-                if node.val < R:
-                    dfs(node.right)
+  # define root right
+  rl = TreeNode(val=13)
+  rr = TreeNode(val=18)
+  r = TreeNode(val=15, left=rl, right=rr)
 
-        self.ans = 0
-        dfs(root)
-        return self.ans
+  # define root
+  root = TreeNode(val=10, left=l, right=r)
 
-#Iterative implementation
-class Solution(object):
-    def rangeSumBST(self, root, L, R):
-        ans = 0
-        stack = [root]
-        while stack:
-            node = stack.pop()
-            if node:
-                if L <= node.val <= R:
-                    ans += node.val
-                if L < node.val:
-                    stack.append(node.left)
-                if node.val < R:
-                    stack.append(node.right)
-        return ans
-'''
+  print(sol.rangeSumBST(root, 7, 15))
