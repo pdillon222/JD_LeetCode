5b94d71a2a77787af3feb74415911cd9cac27cc7
James <pdillon222@gmail.com>
2020-09-12 09:49:07 -0700

    Non-working btree creation, ideas seem plausible however

--- Range_Sum_Of_BST_/py.d/tree_range_of_sum.py
+++ Range_Sum_Of_BST_/py.d/tree_range_of_sum.py
@@ -1,3 +1,5 @@
+from typing import List
+
 # TODO - construct a method to build the tree from given list:
 # [10,5,15,3,7,13,18,1,null,6] => becomes
 # L=7, R=15 => 45
@@ -12,20 +14,42 @@
 '''
 # Definition for a binary tree node.
 class TreeNode:
-  def __init__(self, val=0, left=None, right=None):
-    self.val = val
-    self.left = left
-    self.right = right
+  def __init__(self, val):
+    self.node = val
+    self.left = None
+    self.right = None
+
+  def insert_node(self, val, location=None):
+      if not location:
+        location = TreeNode(val)
+      else:
+        if val > location.node:
+          self.insert_node(val, location.right)
+        else:
+          self.insert_node(val, location.left)
+
+  def ascend_tree(self, node=None):
+    if node == None:
+      node = self
+    if node:
+      self.ascend_tree(node.left)
+      print(node.node)
+      self.ascend_tree(node.right)
 
 class Solution:
+  def __init__(self):
+    pass
+
   def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
     return(root.val, root.left.left.left.val, root.left.right.left.val)
 
 if __name__=="__main__":
   sol = Solution()
-  # [10,5,15,3,7,13,18,1,null,6], L=7, R=15 => 45
+  # [10,5,15,3,7,13,18,1,None,6], L=7, R=15 => 45
 
+  # manual btree definition
   # define root left
+  '''
   ll = TreeNode(val=3, left=TreeNode(val=1))
   lr = TreeNode(val=7, left=TreeNode(val=6))
   l = TreeNode(val=5, left=ll, right=lr)
@@ -37,5 +61,16 @@ if __name__=="__main__":
 
   # define root
   root = TreeNode(val=10, left=l, right=r)
-
-  print(sol.rangeSumBST(root, 7, 15))
+  '''
+  tree_vals = [10,5,15,3,7,13,18,1,None,6]
+  tree_vals = [val for val in tree_vals if type(val) == int]
+  new_tree = TreeNode(tree_vals.pop(0))
+  print(tree_vals)
+  iter_vals = map(lambda x: new_tree.insert_node(x), tree_vals)
+  while True:
+    try:
+      next(iter_vals)
+    except StopIteration:
+      break
+  print(new_tree.node, new_tree.left, new_tree.right)
+  #new_tree.ascend_tree()
