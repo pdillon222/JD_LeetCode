f8b5ee35428b72c70e02d41e5cd17f170165c884
James <pdillon222@gmail.com>
2020-09-23 06:37:30 -0700

    Started work on js BST

--- Range_Sum_Of_BST_/js.d/tree_range_of_sum.js
+++ Range_Sum_Of_BST_/js.d/tree_range_of_sum.js
@@ -1,11 +1,22 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
+// [10,5,15,3,7,13,18,1,null,6] => becomes
+// L=7, R=15 => 45
+/*
+                   10
+                 /    \
+                5     15
+              /   \  /   \
+             3    7 13   18
+           /     /
+          1     6
+*/
+
+//Definition for a binary tree node.
+function TreeNode(val, left, right) {
+  this.val = (val===undefined ? 0 : val)
+  this.left = (left===undefined ? null : left)
+  this.right = (right===undefined ? null : right)
+}
+
 /**
  * @param {TreeNode} root
  * @param {number} L
@@ -13,5 +24,8 @@
  * @return {number}
  */
 var rangeSumBST = function(root, L, R) {
-
+  console.log(root.val);
 };
+
+var tn = TreeNode(4, TreeNode(2), TreeNode(6));
+rangeSumBST(tn, 7, 15);
