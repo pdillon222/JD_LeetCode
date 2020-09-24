c2c108b61ee36d45bb32567e7f31485b6e6a0197
James <pdillon222@gmail.com>
2020-09-24 07:15:35 -0700

    ES6 TreeNode class implementation

--- Range_Sum_Of_BST_/js.d/tree_range_of_sum.js
+++ Range_Sum_Of_BST_/js.d/tree_range_of_sum.js
@@ -9,23 +9,47 @@
            /     /
           1     6
 */
-
-//Definition for a binary tree node.
+// LeetCode definition for a binary tree node.
+/*
 function TreeNode(val, left, right) {
   this.val = (val===undefined ? 0 : val)
   this.left = (left===undefined ? null : left)
   this.right = (right===undefined ? null : right)
 }
+*/
 
-/**
+// ES6 implementation
+class TreeNode {
+  constructor (val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+  }
+  // Node insertion function
+  insertNode (val){
+    if (!this.val){
+      this.val = val;
+    } else {
+      if (val < this.val){
+        // go left
+      } else {
+        // go right
+      }
+    }
+  }
+}
+
+/** LeetCode function - passing in constructed BST
  * @param {TreeNode} root
  * @param {number} L
  * @param {number} R
  * @return {number}
  */
 var rangeSumBST = function(root, L, R) {
-  console.log(root.val);
+    console.log(root);
+    root.insertNode(5);
+    console.log(root);
 };
 
-var tn = TreeNode(4, TreeNode(2), TreeNode(6));
+var tn = new TreeNode(4);
 rangeSumBST(tn, 7, 15);
