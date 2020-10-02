26c66e3c77b7ed2dcfa4ebb7903144c7d7fd35b1
James <pdillon222@gmail.com>
2020-10-02 05:57:46 -0700

    test assertions for longest_substring -> python

--- Longest_Substring_Without_Repeating_Characters_/py.d/longest_substring.py
+++ Longest_Substring_Without_Repeating_Characters_/py.d/longest_substring.py
@@ -1,1 +1,13 @@
-placeholder
+class Solution:
+    def lengthOfLongestSubstring(self, s: str, test_map: dict) -> int:
+        return 3
+
+
+if __name__=="__main__":
+    sol = Solution()
+    test_map = {
+        "abcabcbb": 3,
+        "bbbbb": 1,
+        "pwwkew": 3
+    }
+    print(list(map(lambda _str: sol.lengthOfLongestSubstring(_str, test_map) == test_map[_str], test_map.keys())))
