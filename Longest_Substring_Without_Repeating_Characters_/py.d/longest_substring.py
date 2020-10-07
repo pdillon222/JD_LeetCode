2f35bacfcce24f4618367d7afd478be7e0480da1
James <pdillon222@gmail.com>
2020-10-07 10:40:20 -0700

    refactored arg parsing into separate module

--- Longest_Substring_Without_Repeating_Characters_/py.d/longest_substring.py
+++ Longest_Substring_Without_Repeating_Characters_/py.d/longest_substring.py
@@ -1,31 +1,16 @@
 import cProfile
 import argparse
-from test_assertions import map_cases
+from profiler import arg_func_runner
 
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         max_substr_len = 0
-        left_index = 0
-
-        ''' FAIL -> only compares l_index w/ each char to the right
-        #for i in range(0, len(s)):
-        while left_index < len(s):
-            right_index = left_index + 1
-            dup_char_flag = False
-            print(f'Starting at index -> {left_index} == {s[left_index]}')
-            #for j in range(i+1, len(s)):
-            while right_index < len(s) and not dup_char_flag:
-                print(f'{s[left_index]} => {s[right_index]}')
-                dup_char_flag = s[left_index] == s[right_index]
-                substr_len = len(s[left_index:right_index+1])
-                right_index += 1
-            if not dup_char_flag and max_substr_len < substr_len:
-                print(f'Adding substr -> "{s[left_index:right_index]}"')
-                max_substr_len = substr_len
-            print('\n')
-            left_index += 1
-        '''
+        for i in range(len(s)):
+            for j in range(i+1, len(s)+1):
+                if len(s[i:j]) == len(set(s[i:j])) and \
+                len(s[i:j]) > max_substr_len:
+                    max_substr_len = len(s[i:j])
         print(max_substr_len)
         return max_substr_len
 
@@ -41,16 +26,4 @@ if __name__=="__main__":
         "pwwkew": 3
     }
 
-    if args.test_cases:
-        map_cases(test_map, sol.lengthOfLongestSubstring)
-    else:
-        if args.profile:
-            # run code profiling:
-            #cProfile.runctx('g(x,y)', {'x': n, 'y': m, 'g': func},{})
-            profile_str = " Profiling function code "
-            delim = f'{"=" * (26 + len(profile_str))}'
-            print(f'{delim}\n{"=":13}{profile_str}{" ":12}=\n{delim}')
-        else:
-            func_str = "Running function w/ no args"
-            print(f'{"-" * len(func_str)}\n{func_str}\n{"-" * len(func_str)}')
-            sol.lengthOfLongestSubstring("abcabcbb")
+    arg_func_runner(sol.lengthOfLongestSubstring, test_map, func_arg="abcabcbb")
--- _lib/pylib/profiler.py
+++ _lib/pylib/profiler.py
@@ -1,2 +1,40 @@
-# TODO - place all profile handling in file; make available for import
-# see cinema_seats.py for example
+import argparse
+from test_assertions import map_cases
+from typing import List, Callable, TypeVar
+
+ArgsList = TypeVar('ArgsList')
+
+ARGS_LIST = [
+    '--profile',
+    '--test-cases',
+    '--helper'
+]
+
+def argument_parser() -> ArgsList:
+    parser = argparse.ArgumentParser()
+    for arg in ARGS_LIST:
+        parser.add_argument(arg, action='store_true')
+    print(parser.parse_args())
+    return parser.parse_args()
+
+def arg_func_runner(func: Callable,
+                    test_map: dict,
+                    func_str: str = None,
+                    func_int: int = None,
+                    func_list: List = None,
+                    func_dict: dict = None) -> None:
+    args = argument_parser()
+    if args.test_cases:
+        map_cases(test_map, func)
+    else:
+        if args.profile:
+            # run code profiling:
+            #cProfile.runctx('g(x,y)', {'x': n, 'y': m, 'g': func},{})
+            profile_str = " Profiling function code "
+            delim = f'{"=" * (26 + len(profile_str))}'
+            print(f'{delim}\n{"=":13}{profile_str}{" ":12}=\n{delim}')
+        else:
+            func_str = "Running function w/ no args"
+            print(f'{"-" * len(func_str)}\n{func_str}\n{"-" * len(func_str)}')
+            args_list = 
+            func(*args_list)
