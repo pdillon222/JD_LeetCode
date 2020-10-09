afc0fa2a6516f9ebd927de0e1a6f22a39bfdc767
James <pdillon222@gmail.com>
2020-10-09 06:35:31 -0700

    this is hard

--- Longest_Substring_Without_Repeating_Characters_/py.d/longest_substring.py
+++ Longest_Substring_Without_Repeating_Characters_/py.d/longest_substring.py
@@ -2,55 +2,12 @@ import cProfile
 import argparse
 from profiler import arg_func_runner
 
-''' - Brute force method: fails LeetCode time constraint
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        max_substr_len = 0
-        for i in range(len(s)):
-            for j in range(i+1, len(s)+1):
-                if len(s[i:j]) == len(set(s[i:j])) and \
-                len(s[i:j]) > max_substr_len:
-                    max_substr_len = len(s[i:j])
-        print(max_substr_len)
-        return max_substr_len
-'''
-''' - Index length comparison: fails because only considering distance between
-        indices of repeated characters; need to make sure index range does not
-        have repeating characters other than the specific char being tested
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        # find all repeating chars
-        # create map of repeating chars to list of their indices
-        # for those chars w/ multiple indices:
-          # measure the distance up to the end of the string
-          # see if in that range - there lies repeated chars (by comparing dict values w/ > 1 list entries)
-        max_substr_len = 0
-        repeat_char_indices = {}
-        counter = 0
-        while counter < len(s):
-            if s[counter] not in repeat_char_indices:
-                # initialize key w/ list containing index of char
-                repeat_char_indices[s[counter]] = [counter]
-            else:
-                # append index to repeat_char_indices[s]
-                repeat_char_indices[s[counter]].append(counter)
-            counter += 1
-        if set([len(indices) for indices in repeat_char_indices.values()]) == {1}:
-            max_substr_len = len(s)
-            return max_substr_len
-        for indices in repeat_char_indices.values():
-            counter = 0
-            if len(indices) > 1:
-                while counter < len(indices) - 1:
-                    if indices[counter + 1] - indices[counter] > max_substr_len:
-                        max_substr_len = indices[counter + 1] - indices[counter]
-                    counter += 1
-        return max_substr_len
-'''
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         max_substr_len = 0
+        l_index = 0
+        r_index = len(s) - 1
         repeat_char_indices = {}
         counter = 0
         while counter < len(s):
@@ -61,11 +18,27 @@ class Solution:
                 # append index to repeat_char_indices[s]
                 repeat_char_indices[s[counter]].append(counter)
             counter += 1
+        # if each index list has <= 1 value, no repeat chars -> return len(s)
         if set([len(indices) for indices in repeat_char_indices.values()]) == {1}:
-            max_substr_len = len(s)
-            return max_substr_len
+            return len(s)
         # TODO - do stuff
-
+        for char, indices in repeat_char_indices.items():
+            counter = 0
+            print(f'{char} -> {indices}')
+            # a special case occurs when we get to last element in index array
+            # l_index == index && r_index == len(s) - 1
+            # if ((len(s) - 1) - index > max_substr_len
+            while counter < len(indices):
+                #print(indices[counter])
+                if counter != len(indices) - 1:
+                    print(f'{indices[counter]} -> {indices[counter + 1]}')
+                    # if indices[counter] > l_index and indices[counter + 1] < r_index:
+                        # change the l and r indices and change max_substr_len to diff
+                else:
+                    print(f'comparing index {indices[counter]} w/ end of string')
+                    print(f'{indices[counter]} -> {len(s) - 1}')
+                counter += 1
+            print("--")
         ######
         return max_substr_len
 
@@ -81,7 +54,8 @@ if __name__=="__main__":
         "bbbbb": 1,
         "pwwkew": 3,
         "abcdefg": 7,
-        "aab": 2
+        "aab": 2,
+        "abzdebbfgqbfwzmacb": 9
     }
 
     arg_func_runner(sol.lengthOfLongestSubstring, test_map, func_str="pwwkew")
