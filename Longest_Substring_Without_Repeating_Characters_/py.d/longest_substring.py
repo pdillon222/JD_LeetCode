7e22a2a8defde9cc8075c4f365c4af236b111953
James <pdillon222@gmail.com>
2020-10-10 06:55:33 -0700

    Unsuccessful modifications

--- Longest_Substring_Without_Repeating_Characters_/py.d/longest_substring.py
+++ Longest_Substring_Without_Repeating_Characters_/py.d/longest_substring.py
@@ -8,6 +8,7 @@ class Solution:
         max_substr_len = 0
         l_index = 0
         r_index = len(s) - 1
+        #print(s[l_index:r_index + 1]) # the full string
         repeat_char_indices = {}
         counter = 0
         while counter < len(s):
@@ -21,30 +22,27 @@ class Solution:
         # if each index list has <= 1 value, no repeat chars -> return len(s)
         if set([len(indices) for indices in repeat_char_indices.values()]) == {1}:
             return len(s)
-        # TODO - do stuff
+
         for char, indices in repeat_char_indices.items():
             counter = 0
-            print(f'Repeating char "{char}" at -> {indices}')
-            # iterate through lists of indices for repeat char, compare distance n[0] -> n[1]
+            print(f'"{char}" -> {indices}')
+            # check substring from s[0:indices[0]] - if counter == 0
+            # check substring from s[indices[counter]:indices[counter + 1]]
+            # check substring from s[indices[counter]:len(s)] - if counter == len(indices) - 1
             while counter < len(indices):
-                if counter != len(indices) - 1:
-                    print(f'{indices[counter]} -> {indices[counter + 1]}')
+                if counter == 0:
+                    print(s[0:])
+                    pass
+                elif counter == len(counter) - 1:
+                    pass
                 else:
-                    # if index is last in list; check distance from it to end of string
-                    # determine whether or not duplicate chars are in this range
-                    print(f'comparing index {indices[counter]} w/ end of string')
-                    print(f'{indices[counter]} -> {len(s) - 1}')
-                counter += 1
-            print("--")
-        ######
+                    # compare substring between indices
+                    pass
+
         return max_substr_len
 
 
 if __name__=="__main__":
-    parser = argparse.ArgumentParser()
-    for arg in ['--profile', '--test-cases']:
-        parser.add_argument(arg, action='store_true')
-    args = parser.parse_args()
     sol = Solution()
     test_map = {
         "abcabcbb": 3,
@@ -55,5 +53,4 @@ if __name__=="__main__":
         "abzdebbfgqbfwzmacb": 9
     }
 
-    arg_func_runner(sol.lengthOfLongestSubstring, test_map, func_str="pwwkew")
-    #sol.lengthOfLongestSubstring("")
+    arg_func_runner(sol.lengthOfLongestSubstring, test_map, func_str="abcabcbb")
