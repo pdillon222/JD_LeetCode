be774155529813c7db7e0824c2ab8f6123b2574a
James <pdillon222@gmail.com>
2020-12-24 08:00:24 -0800

    Starting work on longest_substring cpp

--- Longest_Substring_Without_Repeating_Characters_/cpp.d/longest_substring.cpp
+++ Longest_Substring_Without_Repeating_Characters_/cpp.d/longest_substring.cpp
@@ -1,1 +1,32 @@
-placeholder
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+class LongestSubstring {
+public:
+    int lengthOfLongestSubstring(string s){
+        printf("Hello string -> %s\n", s.c_str());
+    }
+};
+
+int main(){
+  LongestSubstring ls;
+  string teststring = "new string";
+  ls.lengthOfLongestSubstring(teststring);
+
+  return 0;
+}
+
+
+/*
+test_map = {
+    "abcabcbb": 3,
+    "bbbbb": 1,
+    "pwwkew": 3,
+    "abcdefg": 7,
+    "aab": 2,
+    "dvdf": 3
+}
+*/
--- Subrectangle_Queries/cpp.d/subrectangle_queries.cpp
+++ Subrectangle_Queries/cpp.d/subrectangle_queries.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
-#include<stdio.h>
-#include<vector>
+#include <iostream>
+#include <stdio.h>
+#include <vector>
 
 using namespace std;
 
