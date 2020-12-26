c82cff7fcad73aa0332d93494f800127a6a400f3
James <pdillon222@gmail.com>
2020-12-26 06:34:07 -0800

    created means for result mapping, will move to it's own class

--- Longest_Substring_Without_Repeating_Characters_/cpp.d/longest_substring.cpp
+++ Longest_Substring_Without_Repeating_Characters_/cpp.d/longest_substring.cpp
@@ -1,32 +1,46 @@
-#include <iostream>
-#include <stdio.h>
 #include <vector>
+#include <stdio.h>
+#include <iostream>
+#include <unordered_map>
 
-using namespace std;
+/*
+- colored output -> https://stackoverflow.com/questions/9158150/colored-output-in-c/9158263
+*/
+
+typedef std::unordered_map<std::string, int> expected_result;
 
 class LongestSubstring {
 public:
-    int lengthOfLongestSubstring(string s){
-        printf("Hello string -> %s\n", s.c_str());
+    int lengthOfLongestSubstring(std::string s){
+        return 3;
+    }
+
+    bool function_validate(std::string s, int res){
+      printf("Checking string -> %s\n", s.c_str());
+      bool bool_result = this->lengthOfLongestSubstring(s) == res;
+      if (bool_result){
+        std::cout << "string " <<
+        s << " yields correct result -> " <<
+        res << "\n\n" ;
+      } else {
+        std::cout << "bool result no likey\n\n";
+      }
+    return bool_result;
     }
 };
 
 int main(){
   LongestSubstring ls;
-  string teststring = "new string";
-  ls.lengthOfLongestSubstring(teststring);
-
+  expected_result results({
+    {"abcabcbb", 3},
+    {"bbbbb", 1},
+    {"pwwkew", 3},
+    {"abcdefg", 7},
+    {"aab", 2},
+    {"dvdf", 3}
+  });
+  for (auto x: results){
+    ls.function_validate(x.first, x.second);
+  }
   return 0;
 }
-
-
-/*
-test_map = {
-    "abcabcbb": 3,
-    "bbbbb": 1,
-    "pwwkew": 3,
-    "abcdefg": 7,
-    "aab": 2,
-    "dvdf": 3
-}
-*/
