605fac592d1040b5c919e2b2c2af83170e87ab15
James <pdillon222@gmail.com>
2021-01-10 09:12:57 -0800

    prep for rework of test_assertions - need to parse new results mapping data structure

--- lib/pylib/test_assertions.py
+++ lib/pylib/test_assertions.py
@@ -5,7 +5,16 @@ from typing import Callable
 
 # Callable[[Arg1Type, Arg2Type], ReturnType]
 
-def map_cases(test_map: dict, func: Callable[[type(abs)], None], brute_force: bool = False) -> None:
+'''
+ TODO - rework to use new map data structure:
+ considerations:
+   - list of tuples -> (result, [args])
+   - list of dicts -> {'res': val, 'args': [args]}
+ construct a helper function - for creating the data structure:
+   - func(res_list: list, args_list: list[list])
+'''
+
+def map_cases(test_map: dict, func: Callable[[type(abs)], None]) -> None:
     for k, v in test_map.items():
         func_result = func(k, brute_force) if brute_force else func(k)
         uni_char = GREEN_CHECK if func_result == test_map[k] else RED_OCT
