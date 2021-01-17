ce6e64a0162b05c6bada959727e3b61f6d11d0ad
James <pdillon222@gmail.com>
2021-01-17 10:38:59 -0800

    edge case that breaks code

--- Remove_K_Digits_/py.d/remove_k_digits.py
+++ Remove_K_Digits_/py.d/remove_k_digits.py
@@ -21,11 +21,16 @@ class Solution:
                 k -= 1
             index += 1
         # edge cases:
-        # - len(remove_indices) == 0
-        # - len(remove_indices) < k
-        reduced_string =  ''.join([num[index] for index in range(len(num))
+        # - len(remove_indices) == 0 # indicates all nums are ascending -> thus we chop off the end
+        # - len(remove_indices) < k # indicates remaining chars are ascending -> thus we chop off the end
+
+        # if k leftover - we simply subtract it from remaining range
+
+        reduced_string =  ''.join([num[index] for index in range(len(num) - k)
                                   if index not in remove_indices])
-        print(reduced_string)
+        zero_check = all([_char == '0' for _char in reduced_string])
+        if zero_check:
+            return '0'
         return reduced_string.lstrip('0')
 
 
@@ -34,7 +39,8 @@ if __name__=="__main__":
     test_map = [
         ('1219', ['1432219', 3]),
         ('200', ['10200', 1]),
-        ('0', ['10', 2])
+        ('0', ['10', 2]),
+        ('0', ['1234567890', 9]) # problem
     ]
     test_mapII = [
         ('2043', ['4325043', 3]),
