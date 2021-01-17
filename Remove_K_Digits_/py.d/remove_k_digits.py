78f8d8dc04ab2ec02d7412c8d0d310c8a533a083
James <pdillon222@gmail.com>
2021-01-17 10:21:13 -0800

    initial test-cases working - need to add conditions for edge cases

--- Remove_K_Digits_/py.d/remove_k_digits.py
+++ Remove_K_Digits_/py.d/remove_k_digits.py
@@ -12,20 +12,33 @@ class Solution:
         n = len(num)
         if k >= n: return '0'
         if k == 0: return num
-        num_stack = []
-
+        remove_indices = []
+        index = 0
+        while index < n - 1 and k:
+            if int(num[index]) > int(num[index + 1]):
+                print(f'removing index {index} -> "{num[index]}"')
+                remove_indices.append(index)
+                k -= 1
+            index += 1
+        # edge cases:
+        # - len(remove_indices) == 0
+        # - len(remove_indices) < k
+        reduced_string =  ''.join([num[index] for index in range(len(num))
+                                  if index not in remove_indices])
+        print(reduced_string)
+        return reduced_string.lstrip('0')
 
 
 if __name__=="__main__":
     sol = Solution()
     test_map = [
-        ('1219', [3, '1432219']),
-        ('200', [1, '10200']),
-        ('0', [2, '10'])
+        ('1219', ['1432219', 3]),
+        ('200', ['10200', 1]),
+        ('0', ['10', 2])
     ]
     test_mapII = [
-        ('2043', [3, '4325043']),
-        ('221', [5, '765028321']),
-        ('1118', [2, '121198'])
+        ('2043', ['4325043', 3]),
+        ('221', ['765028321', 5]),
+        ('1118', ['121198', 2])
     ]
     arg_func_runner(sol.removeKdigits, args=['16374112', 3], map=test_map)  # "13112"
