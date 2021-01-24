67249c1ef6e8855d25c9a32ef7d506a9f164a81a
James <pdillon222@gmail.com>
2021-01-24 06:44:49 -0800

    test cases failing where remaining chars to be removed are left

--- Remove_K_Digits_/py.d/remove_k_digits.py
+++ Remove_K_Digits_/py.d/remove_k_digits.py
@@ -13,11 +13,11 @@ class Solution:
         if k >= n: return '0'
         if k == 0: return num
         remove_indices = []
-        index = 0
-        while index < n - 1 and k:
-            if int(num[index]) > int(num[index + 1]):
-                print(f'removing index {index} -> "{num[index]}"')
-                remove_indices.append(index)
+        index = 1
+        while index < n  and k:
+            if int(num[index - 1]) > int(num[index]):
+                print(f'removing index {index - 1} -> "{num[index - 1]}"')
+                remove_indices.append(index - 1)
                 k -= 1
             index += 1
         # edge cases:
@@ -26,8 +26,9 @@ class Solution:
 
         # if k leftover - we simply subtract it from remaining range
         print(f'remaining value of k -> {k}')
-        reduced_string =  ''.join([num[index] for index in range(len(num) - k)
-                                  if index not in remove_indices])
+        string_range = range(k, len(num)) if k else range(len(num) - k)
+        reduced_string = ''.join([num[index] for index in string_range
+                                 if index not in remove_indices])
         zero_check = all([_char == '0' for _char in reduced_string])
         if zero_check:
             return '0'
@@ -40,11 +41,12 @@ if __name__=="__main__":
         ('1219', ['1432219', 3]),
         ('200', ['10200', 1]),
         ('0', ['10', 2]),
-        ('0', ['1234567890', 9]) # problem
+        ('0', ['1234567890', 9]), # problem
+        ('11', ['112', 1])
     ]
     test_mapII = [
         ('2043', ['4325043', 3]),
         ('221', ['765028321', 5]),
         ('1118', ['121198', 2])
     ]
-    arg_func_runner(sol.removeKdigits, args=['16374112', 3], map=test_map)  # "13112"
+    arg_func_runner(sol.removeKdigits, args=['1234567890', 9], map=test_map[-2:])  # "0"
