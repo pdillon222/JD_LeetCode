cb8c0dbf68b07ded8d2afc2c8d2821ec791f3145
James <pdillon222@gmail.com>
2021-01-31 08:15:11 -0800

    working through Python recursive solution remove_k_digits

--- Remove_K_Digits_/py.d/remove_k_digits.py
+++ Remove_K_Digits_/py.d/remove_k_digits.py
@@ -5,34 +5,25 @@ from stdout import GREEN_CHECK, RED_OCT
 
 
 class Solution:
-    def removeKdigits(self, num: str, k: int) -> str:
-        # if num in ascending order -> remove last k digits
-        # conversely - in descending order -> first k digits
-        # everything else
-        n = len(num)
-        if k >= n: return '0'
-        if k == 0: return num
-        remove_indices = []
-        index = 1
-        while index < n  and k:
-            if int(num[index - 1]) > int(num[index]):
-                print(f'removing index {index - 1} -> "{num[index - 1]}"')
-                remove_indices.append(index - 1)
-                k -= 1
-            index += 1
-        # edge cases:
-        # - len(remove_indices) == 0 # indicates all nums are ascending -> thus we chop off the end
-        # - len(remove_indices) < k # indicates remaining chars are ascending -> thus we chop off the end
+    @classmethod
+    def buildLowestNumString(cls, str_num: str, n: int, res: str) -> str:
+        if n == 0: return str_num
+        length = len(str_num)
+        if length <= n: return res
+        min_index = 0
+        for i in range(1, n):
+            if str_num[i] < str_num[min_index]: min_index = i
+        res += str_num[min_index]
+        print(f'new value of res_str -> {res}')
+        substring = str_num[min_index + 1:]
+        print(f'new substring -> {substring}')
+        return __class__.buildLowestNumString(substring, n - min_index, res)
 
-        # if k leftover - we simply subtract it from remaining range
-        print(f'remaining value of k -> {k}')
-        string_range = range(k, len(num)) if k else range(len(num) - k)
-        reduced_string = ''.join([num[index] for index in string_range
-                                 if index not in remove_indices])
-        zero_check = all([_char == '0' for _char in reduced_string])
-        if zero_check:
-            return '0'
-        return reduced_string.lstrip('0')
+    def removeKdigits(self, num: str, k: int) -> str:
+        result = __class__.buildLowestNumString(num, k, "")
+        print(result)
+        while result[0] == '0': result = result[1:]
+        return '0' if result == '' else result
 
 
 if __name__=="__main__":
@@ -49,4 +40,4 @@ if __name__=="__main__":
         ('221', ['765028321', 5]),
         ('1118', ['121198', 2])
     ]
-    arg_func_runner(sol.removeKdigits, args=['1234567890', 9], map=test_map)#[-2:])  # "0"
+    arg_func_runner(sol.removeKdigits, args=['1234567890', 9], map=test_map[0:1])#[-2:])  # "0"
