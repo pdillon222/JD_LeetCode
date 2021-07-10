f43a4b39828b1d070bbc02fb4761b7e5ebcdebb6
James <pdillon222@gmail.com>
2021-07-10 06:16:50 -0700

    completed remove_k_digits

--- Remove_K_Digits/js.d/remove_k_digits.js
+++ Remove_K_Digits/js.d/remove_k_digits.js
@@ -1,15 +1,17 @@
 var removeKdigits = function(num, k) {
     let result = [];
     for (i in num){
-        while (k > 0 & result.length > 0 & result[-1] > num[i]){
-            result.pop;
+        while (k > 0 & result.length > 0 & result[result.length -1] > num[i]){
+            console.log(`popping ${num[i]}`)
+            result.pop();
             k -= 1;
         }
-        console.log(`pushing ${num[i]}`);
         result.push(num[i]);
     }
-    return result;
+    while (result[0] == '0'){result.shift()};
+    result = result.join("").slice(0, result.length - k);
+    return (result ? result : '0');
 };
 
-let res = removeKdigits('1432219', 3); // 1219
+let res = removeKdigits('1432219', 8); // 1219
 console.log(res)
